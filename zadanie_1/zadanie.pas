61e33b901e5a3253a8057bc5f3467ba97d24ae1e
Konrad Wyka <konrad.wyka@externals.hilti.com>
2024-03-07 17:16:14 +0100

    zadanie 1 4.0

--- zadanie_1/zadanie.pas
+++ zadanie_1/zadanie.pas
@@ -1,29 +1,29 @@
-program RandomNumbersSort;
+program RandomNumbersRange;
 
 uses
   SysUtils;
 
 const
-  MAX_NUMBERS = 50;
+  MAX_NUMBERS = 1000;
 
 type
   NumberArray = array[1..MAX_NUMBERS] of Integer;
 
-procedure GenerateRandomNumbers(var numbers: NumberArray);
+procedure GenerateRandomNumbers(var numbers: NumberArray; minValue, maxValue, count: Integer);
 var
   i: Integer;
 begin
   Randomize;
-  for i := 1 to MAX_NUMBERS do
-    numbers[i] := Random(101);
+  for i := 1 to count do
+    numbers[i] := Random(maxValue - minValue + 1) + minValue;
 end;
 
-procedure SortNumbers(var numbers: NumberArray);
+procedure SortNumbers(var numbers: NumberArray; count: Integer);
 var
   i, j, temp: Integer;
 begin
-  for i := 1 to MAX_NUMBERS - 1 do
-    for j := i + 1 to MAX_NUMBERS do
+  for i := 1 to count - 1 do
+    for j := i + 1 to count do
       if numbers[i] > numbers[j] then
       begin
         temp := numbers[i];
@@ -32,20 +32,28 @@ begin
       end;
 end;
 
-procedure DisplayNumbers(const numbers: NumberArray);
+procedure DisplayNumbers(const numbers: NumberArray; count: Integer);
 var
   i: Integer;
 begin
   writeln('Posortowane liczby:');
-  for i := 1 to MAX_NUMBERS do
+  for i := 1 to count do
     writeln('Liczba ', i, ': ', numbers[i]);
 end;
 
 var
   randomNumbers: NumberArray;
+  minValue, maxValue, count: Integer;
 
 begin
-  GenerateRandomNumbers(randomNumbers);
-  SortNumbers(randomNumbers);
-  DisplayNumbers(randomNumbers);
+  minValue := 0;
+  maxValue := 1000;
+  count := 99;
+
+  if count > MAX_NUMBERS then
+    count := MAX_NUMBERS;
+
+  GenerateRandomNumbers(randomNumbers, minValue, maxValue, count);
+  SortNumbers(randomNumbers, count);
+  DisplayNumbers(randomNumbers, count);
 end.
